a516b3c92ea7991bad82d55a4fcbe25a51f660da
Margarita Degtyareva <dmyletters@gmail.com>
2020-09-27 23:12:14 +0300

    Read words from file option added

--- Hangman.py
+++ Hangman.py
@@ -1,19 +1,30 @@
 import random
 
-words = ["hangman", "agile", "method", "category"]
+
+def read_words(filename):
+    file = open(filename)
+    words = file.readlines()
+    return words
 
 
 class Hangman:
-    word = words[random.randrange(0, 4, 1)]
-    count_letters = len(word)
-    masked_word = '*' * count_letters
-    print(masked_word)
+    standart_filename = "words.txt"
     attempts = 5
     mistakes = 0
 
+    def __init__(self, filename=standart_filename, word=None):
+        if filename is not None:
+            self.words = read_words(filename)
+
+        self.word = self.words[random.randrange(0, len(self.words), 1)]\
+            .strip().lower()
+        self.count_letters = len(self.word)
+        self.masked_word = '*' * self.count_letters
+        print(self.masked_word)
+
     def play(self):
-        while self.mistakes <= self.attempts and not self.is_word_opened():
-            letter = input("Guess a letter: ")
+        while self.mistakes < self.attempts and not self.is_word_opened():
+            letter = input("Guess a letter: ").lower()
             if len(letter) == 1:
                 self.check_letter(letter)
             else:
@@ -27,7 +38,8 @@ class Hangman:
             self.masked_word = self.word
         else:
             self.mistakes += 1
-            print("Missed, mistake " + str(self.mistakes) + " out of " + str(self.attempts) + ".")
+            print("Missed, mistake " + str(self.mistakes)
+                  + " out of " + str(self.attempts) + ".")
         print("The word: " + self.masked_word)
 
     def check_letter(self, letter):
@@ -35,18 +47,19 @@ class Hangman:
             print("Hit!")
             index = self.word.find(letter)
             while index != -1:
-                self.masked_word = self.masked_word[:index] + letter + self.masked_word[index + 1:]
+                self.masked_word = self.masked_word[:index] \
+                                   + letter + self.masked_word[index + 1:]
                 index = self.word.find(letter, index + 1)
             if self.is_word_opened():
                 print("You won!")
         else:
             self.mistakes += 1
-            print("Missed, mistake " + str(self.mistakes) + " out of " + str(self.attempts) + ".")
+            print("Missed, mistake " + str(self.mistakes)
+                  + " out of " + str(self.attempts) + ".")
         print("The word: " + self.masked_word)
 
     def is_word_opened(self):
         return self.masked_word == self.word
-
 
 
 game = Hangman()
